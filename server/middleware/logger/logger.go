ef06d42a8dafc41e7e733349061dcaed0863d445
Roeeeee <35409124+5c077m4n@users.noreply.github.com>
2026-04-05 10:10:08 +0300

    Add group to middleware logging

--- server/middleware/logger/logger.go
+++ server/middleware/logger/logger.go
@@ -8,26 +8,28 @@ import (
 	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
+var logger = slog.Default().WithGroup("mcp_server_middleware")
+
 func New() mcp.Middleware {
 	return func(next mcp.MethodHandler) mcp.MethodHandler {
 		return func(ctx context.Context, method string, req mcp.Request) (mcp.Result, error) {
 			start := time.Now()
 			sessionID := req.GetSession().ID()
 
-			slog.Info("REQUEST", "session", sessionID, "method", method)
+			logger.Info("REQUEST", "session_id", sessionID, "method", method)
 			result, err := next(ctx, method, req)
 			duration := time.Since(start)
 
 			if err != nil {
-				slog.Info(
+				logger.Info(
 					"RESPONSE",
-					"session", sessionID,
+					"session_id", sessionID,
 					"method", method,
 					"duration", duration,
 					"error", err,
 				)
 			} else {
-				slog.Info("RESPONSE", "session", sessionID, "method", method, "duration", duration)
+				logger.Info("RESPONSE", "session_id", sessionID, "method", method, "duration", duration)
 			}
 
 			return result, err
